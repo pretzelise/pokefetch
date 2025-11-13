5039792e59279a89e291ab795fa76ddaf700e132
pretzelise <mail@elise.gg>
2025-11-13 21:12:08 +1000

    Updated example script to use base64 encoded sound effect

--- examples/pokefetch-shiny-sound.sh
+++ examples/pokefetch-shiny-sound.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+sound_file="$HOME/.config/pokefetch/shiny.wav.b64"
+
 { error=$(pokefetch --verbose 2>&1 1>&$out); } {out}>&1
 
 if $(echo $error | grep 'shiny_roll: 0' >/dev/null); then
-  ffplay -nodisp -autoexit $HOME/.config/potd/shiny.wav -loglevel quiet >/dev/null &
+  base64 --decode "$sound_file" | ffplay -nodisp -autoexit -i - -loglevel quiet >/dev/null &
   disown
 fi
